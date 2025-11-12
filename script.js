895fd107ebb445acb0f7e65d6b132a27c6df7c5c
Sunita Jatav <sunitajatav625@gmail.com>
2025-11-12 18:15:55 +0530

    Resigned Website ShuddhSwad

---
+++ script.js
@@ -0,0 +1,266 @@
+
+function locoSroll() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+
+
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+
+}
+
+locoSroll()
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+function cursorEffect() {
+    var page1Content = document.querySelector("#page1-content")
+    var cursor = document.querySelector("#cursor")
+
+    page1Content.addEventListener("mousemove", function (dets) {
+        gsap.to(cursor, {
+            x: dets.x,
+            y: dets.y
+        })
+    })
+
+    page1Content.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 1
+        })
+    })
+
+    page1Content.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0
+        })
+    })
+}
+
+cursorEffect()
+
+
+
+function page2Animation() {
+    gsap.from(".elem h1,h2", {
+        y: 130,
+        opacity: 0,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#page2",
+            scroller: "#main",
+            start: "top 40%",
+            end: "top 40%",
+            //   markers: true,
+            scrub: 2
+        }
+    });
+}
+
+page2Animation();
+
+
+function prodAnimation() {
+    gsap.from("#page3-elements .box", {
+        y: 100,
+        opacity: 0,
+        scale: 0.9,
+        duration: 0.8,
+        ease: "back.out(1)",
+        stagger: 0.25,
+        scrollTrigger: {
+            trigger: "#page3-elements",
+            scroller: "#main",
+            start: "top 80%",
+            end: "top 40%",
+            scrub: false,
+            markers: false,
+            toggleActions: "play none none reverse"
+        }
+    });
+}
+
+prodAnimation()
+
+function silderAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        speed: 1000,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    const swiperEl = document.querySelector('.swiper');
+
+    swiperEl.addEventListener('mouseenter', () => {
+        swiper.autoplay.stop();
+    });
+
+    swiperEl.addEventListener('mouseleave', () => {
+        swiper.autoplay.start();
+    });
+
+}
+
+silderAnimation()
+
+function startPageAnimation(){
+    var tl = gsap.timeline()
+
+tl.from("#loader h3", {
+    x: 40,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.1
+})
+tl.to("#loader h3", {
+    opacity: 0,
+    x: -10,
+    duration: 1,
+    stragger: 0.1
+})
+tl.to("#loader", {
+    opacity: 0
+})
+
+
+tl.from("#page1-content h1 span", {
+    y: 100,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 0.5,
+    delay: -0.5
+})
+
+tl.to("#loader", {
+    display: "none"
+})
+}
+
+
+startPageAnimation()
+
+
+
+// LEFT SIDE animation
+gsap.to("#page5-left h6, #page5-left h2, #page5-left > div", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    stagger: 0.2,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: "#page5-left",
+        scroller: "#main", 
+        start: "top 80%",
+        end: "top 50%",
+        scrub: false,
+
+    }
+});
+
+// RIGHT SIDE animation
+gsap.to("#page5-right h2, #page5-right > p, #page5-right > div", {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    stagger: 0.2,
+    delay: 0.2,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: "#page5-right",
+        scroller: "#main", 
+        start: "top 80%",
+        end: "top 50%",
+        scrub: false,
+
+    }
+});
+
+//Page6 Animation
+gsap.from("#page6 img", {
+    y: 20,             
+    opacity: 0,
+    scale: 0.95,      
+    ease: "bounce.out", 
+    duration: 0.8,     
+    stagger: 0.15,     
+    scrollTrigger: {
+        trigger: "#page6",
+        scroller: "#main", 
+        start: "top 80%",
+        toggleActions: "play none none reverse",
+    }
+});
+
+
+
+// Animate footer links
+gsap.to("#footer .footer-links div", {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: "power3.out",
+    stagger: 0.2,
+    scrollTrigger: {
+        trigger: "#footer",
+        scroller: "#main",
+        start: "top 85%",
+        toggleActions: "play none none reverse",
+    }
+});
+
+// Animate footer bottom
+gsap.to("#footer .footer-bottom", {
+    opacity: 1,
+    y: 0,
+    duration: 1,
+    ease: "power3.out",
+    scrollTrigger: {
+        trigger: "#footer",
+        scroller: "#main",
+        start: "top 95%",
+        toggleActions: "play none none reverse",
+    }
+});
